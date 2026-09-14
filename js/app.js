6b818b5c176a40f9dc26c154c1a2cf2df00548f6
tcbarisgencoglu-png <tcbarisgencoglu-png@users.noreply.github.com>
2026-09-14 12:24:57 +0300

    release: v1.0.8 with student photo compression and auto-updater

--- js/app.js
+++ js/app.js
@@ -2610,7 +2610,7 @@ function initApp() {
 }
 
 // Mevcut uygulama sürümü (her güncellemede değişir)
-const APP_VERSION = '1.0.7';
+const APP_VERSION = '1.0.8';
 
 // GitHub & Tauri Auto-Updater kontrolü
 async function checkForUpdates() {
